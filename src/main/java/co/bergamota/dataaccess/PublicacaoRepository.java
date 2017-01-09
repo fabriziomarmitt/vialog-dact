19679adffa4ec1bd62b06b24617732853fa503a0
Fabrizio Marmitt <fabrizio.marmitt@zerohora.com.br>
2017-01-09 19:40:04 -0200

    ajuste solr max results e url via conf

--- src/main/java/co/bergamota/dataaccess/PublicacaoRepository.java
+++ src/main/java/co/bergamota/dataaccess/PublicacaoRepository.java
@@ -6,19 +6,34 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocumentList;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class PublicacaoRepository
 {
+
+    String solrUrl;
+    int solrMaxResults;
+
+    public PublicacaoRepository(){
+        Properties prop = new Properties();
+        try {
+            prop.load(this.getClass().getClassLoader().getResourceAsStream("application.properties"));
+            solrUrl = prop.getProperty("solr.url");
+            solrMaxResults = Integer.parseInt(prop.getProperty("solr.maxResults"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public List<Publicacao> search(String q){
-        String urlString = "http://10.40.146.42:8983/solr/vialog";
-        SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+        SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
 
         SolrQuery query = new SolrQuery();
+        query.setRows(solrMaxResults);
         query.setQuery(String.format("search:%s", q));
         try {
             QueryResponse response = solr.query(query);
